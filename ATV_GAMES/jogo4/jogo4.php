4b828f2462e5d256436bd3fd04d3f8414ece5d7f
GusThyerris <gustavothyerrisdonascimentooli@gmail.com>
2023-10-02 23:08:53 -0300

    update

---
+++ ATV_GAMES/jogo4/jogo4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Jogo da Velha Aninhado</title>
+</head>
+
+<body>
+
+    <button type="submit">Jogar</button>
+    <button type="submit">Jogar</button>
+    <button type="submit">Jogar</button>
+    <button type="submit">Jogar</button>
+    <button type="submit">Jogar</button>
+    <button type="submit">Jogar</button>
+    <button type="submit">Jogar</button>
+    <button type="submit">Jogar</button>
+
+</body>
+
+</html>
